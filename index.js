1fafe8234c0805d9aac4fd13a08c16aa953f4600
Let NON <81377915+NONTorials@users.noreply.github.com>
2021-09-01 18:22:03 +0000

    Arrojamiento de errores añadido

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-
-
 var nums = '0123456789',
     letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
     especial = '!$%^&*()_+|~-=`{}[]:;<>?,./';
 
-
+const RandomKeyError = require('./error');
 
 function construct (config) {
   var chars = '';
-  if (config.numeric) { chars += nums; }
-  if (config.letters) { chars += letras; }
-  if (config.special) { chars += especial; }
-  for (var i = 0; i <= config.exclude.length; i++){
-    chars = chars.replace(config.exclude[i], "");
+  if (config.numero) { chars += nums; }
+  if (config.letras) { chars += letras; }
+  if (config.extras) { chars += especial; }
+  for (var i = 0; i <= config.excluir.length; i++){
+    chars = chars.replace(config.excluir[i], "");
   }
   return chars;
 };
@@ -20,23 +18,32 @@ function construct (config) {
 function base (config) {
   config || (config = {});
   return {
-    largo: config.length || 5,
-    numero: typeof config.numeric === 'boolean' ? config.numeric : true,
-    letras: typeof config.letters === 'boolean' ? config.letters : true,
-    extras: typeof config.special === 'boolean' ? config.special : false,
-    excluir: Array.isArray(config.exclude)       ? config.exclude : []
+    largo: config.largo || 5,
+    numero: typeof config.numero === 'boolean' ? config.numero : true,
+    letras: typeof config.letras === 'boolean' ? config.letras : true,
+    extras: typeof config.extras === 'boolean' ? config.extras : false,
+    excluir: Array.isArray(config.excluir)       ? config.excluir : []
   };
 };
 
 
 
 
-module.exports = function randomString(config) {
+module.exports = function randomKey(config) {
+if(isNaN(config.largo) && config.largo){
+throw new RandomKeyError('El numero de longitud para la key debe ser un numero y se proporcionó: "'+config.largo+'"')
+}
+if(!Array.isArray(config.excluir) && config.excluir){
+ throw new RandomKeyError('config.excluir Debe ser un array y se proporcionó: "'+typeof config.excluir+'"') 
+}
+
+
   config = base(config);
+
   var i, rn,
       rnd = '',
-      len = config.length,
-      exclude = config.exclude,
+      len = config.largo,
+      exclude = config.excluir,
       randomChars = construct(config);
   for (i = 1; i <= len; i++) {
     rnd += randomChars.substring(rn = Math.floor(Math.random() * randomChars.length), rn + 1);
