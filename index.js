f855eac2849b6b4c3bbc2fd80ef56db2718ad23f
Let NON <81377915+NONTorials@users.noreply.github.com>
2021-09-01 15:46:38 +0000

    Bugs reparados, version estable

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-let nums = "0123456789", 
-lets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-extras = "!$%^&*()_+|~-=`{}[]:;<>?,./";
 
-function neu (config){
-config || (config = {})
 
- return {
-    largo: config.largo || 5,
-    nums: typeof config.nums === 'boolean' ? config.nums : true,
-    letras: typeof config.letras === 'boolean' ? config.letras : true,
-    extras: typeof config.extras === 'boolean' ? config.extras : false,
-    excluir: Array.isArray(config.excluir)       ? config.excluir : []
-  };
+var nums = '0123456789',
+    letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+    especial = '!$%^&*()_+|~-=`{}[]:;<>?,./';
 
 
 
-};
-function construct(config){
- var carac = '';
-  if (config.nums) { carac += numbers; }
-  if (config.letras) { carac += letters; }
-  if (config.extras) { carac += specials; }
-  for (var i = 0; i <= config.excluir.length; i++){
-    carac = carac.replace(config.excluir[i], "");
+function construct (config) {
+  var chars = '';
+  if (config.numeric) { chars += nums; }
+  if (config.letters) { chars += letras; }
+  if (config.special) { chars += especial; }
+  for (var i = 0; i <= config.exclude.length; i++){
+    chars = chars.replace(config.exclude[i], "");
   }
-  return carac;
-
+  return chars;
 };
 
-module.exports = function randomKey(config) {
-opts = neu(config);
+function base (config) {
+  config || (config = {});
+  return {
+    largo: config.length || 5,
+    numero: typeof config.numeric === 'boolean' ? config.numeric : true,
+    letras: typeof config.letters === 'boolean' ? config.letters : true,
+    extras: typeof config.special === 'boolean' ? config.special : false,
+    excluir: Array.isArray(config.exclude)       ? config.exclude : []
+  };
+};
+
+
+
+
+module.exports = function randomString(config) {
+  config = base(config);
   var i, rn,
       rnd = '',
-      len = config.largo,
-      exclude = config.excluir,
-      random = construct(config);
+      len = config.length,
+      exclude = config.exclude,
+      randomChars = construct(config);
   for (i = 1; i <= len; i++) {
-    rnd += random.substring(rn = Math.floor(Math.random() * random.length), rn + 1);
+    rnd += randomChars.substring(rn = Math.floor(Math.random() * randomChars.length), rn + 1);
   }
   return rnd;
-
-
-
 };
+
